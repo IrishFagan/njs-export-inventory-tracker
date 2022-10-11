2b9c43a347d3e36b6dd2c518cc35b05186cddbb9
IrishFagan <irishfagan@live.com>
2022-10-10 22:57:17 -0700

    Refactor functionality to support not just frame items

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ export default function App() {
 
   useEffect(() => {
   
-    const getFrameCount = async () => {
+    const getItemCount = async (itemName) => {
       return axios
-        .get("https://www.njs-export.com/collections/frames.json")
+        .get(`https://www.njs-export.com/collections/${itemName}.json`)
         .then(res => res.data.collection.products_count);
     }
     
-    const getFrameInfo = async () => {
-      const pages = await Math.ceil(await getFrameCount() / 30);
-      setFrames([])
+    const getItemInfo = async (itemName, item, setItem) => {
+      const pages = await Math.ceil(await getItemCount(itemName) / 30);
+      setItem([])
       for(let i = 1; i <= pages; i++) {
-        const response = await axios.get(`https://www.njs-export.com/collections/frames/products.json?page=${i}`)
-        const filteredResponse = response.data.products
-        setFrames(frames => [...frames, ...filteredResponse])
+        const response = await axios.get(`https://www.njs-export.com/collections/${itemName}/products.json?page=${i}`)
+        const filteredResponse = response.data.products;
+        setItem(item => [...item, ...filteredResponse])
       }
     }
 
-    getFrameInfo();
+    getItemInfo('frames', frames, setFrames);
   }, [])
 
   return(
