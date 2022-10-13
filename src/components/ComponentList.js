698970965e7c712beaa813a7f7bb09e5384e306c
IrishFagan <irishfagan@live.com>
2022-10-13 09:05:55 -0700

    Add functionality to show current listing date

--- src/components/ComponentList.js
+++ src/components/ComponentList.js
@@ -3,15 +3,15 @@ import Component from './Component';
 export default function ComponentList({componentName, component, todayDate}) {
 	return(
 		<div>
-			<h2>{componentName}</h2>
+			<h2>{componentName} - {new Date(todayDate).toString()}</h2>
     	<ul>
-    	  {component.map((individualComponent, i) =>
-    	    <Component
-    	      component={individualComponent}
-    	      index={i}
-    	      todayDate={todayDate}
-    	    />
-    	  )}
+    	{component.map((individualComponent, i) =>
+    	  <Component
+    	    component={individualComponent}
+    	    index={i}
+    	    todayDate={todayDate}
+    	  />
+    	)}
     	</ul>
     </div>
 	)
