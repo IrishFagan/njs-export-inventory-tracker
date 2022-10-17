d6b984cd6bf466024d1f59c42ab713a791fbe912
IrishFagan <irishfagan@live.com>
2022-10-17 09:05:32 -0700

    Add regex to Date constructor to reduce inconsistincies between formats

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,16 +1,17 @@
 export default function Component({component, index, listingDate}) {
 
 	const CreatedDate = (component) => {
-	  const date = new Date(component.created_at)
-	  return date.setHours(0,0,0,0);
+	  const date = new Date(component.created_at.replace(/-/g, '\/').replace(/T.+/, ''))
+	  return date.toDateString();
 	}
 
-	if(CreatedDate(component) === listingDate.setHours(0,0,0,0)) {
+	if(CreatedDate(component) === listingDate.toDateString()) {
+	  console.log(`${CreatedDate(component)} - ${listingDate.toDateString()}`)
 		return(
 			<li key={index}>
   		  <img src={component.images[0].src} alt="Frame" width={250} height={200} />
   		  <div>
-  		  	{component.tags[1]}
+  		   	{component.tags[1]}
   		  </div>
   		  {component.tags[0]}
   		</li>
