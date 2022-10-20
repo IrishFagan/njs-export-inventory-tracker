690f05d4c93d64bb93f3246cac099645d18ac3d9
IrishFagan <irishfagan@live.com>
2022-10-19 17:26:47 -0700

    Modify header for bike component list

--- src/components/ComponentList.js
+++ src/components/ComponentList.js
@@ -3,7 +3,7 @@ import Component from './Component';
 export default function ComponentList({componentName, component, listingDate}) {
 	return(
 		<div>
-			<h2>{componentName} - {listingDate.toDateString()}</h2>
+			<h2>{componentName}</h2>
     	<ul>
     	{component.map((individualComponent, i) =>
     	  <Component
