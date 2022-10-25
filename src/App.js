ee7b20838082b5248ac663c27c283977448d507f
IrishFagan <irishfagan@live.com>
2022-10-25 14:06:39 -0700

    Remove unnecessary 'getComponentCount' function

--- src/App.js
+++ src/App.js
@@ -18,12 +18,6 @@ export default function App() {
 
   useEffect(() => {
     
-    const getComponentCount = async (componentName) => {
-      return axios
-        .get(`https://xdooj1w64j.execute-api.us-west-2.amazonaws.com/${componentName}/count`)
-        .then(res => res.data.count);
-    }
-    
     const getComponentInfo = async (componentName, component, setComponent) => {
       return axios
         .get('https://xdooj1w64j.execute-api.us-west-2.amazonaws.com/frames')
