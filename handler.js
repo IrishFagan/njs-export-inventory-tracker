976d2ebc24dc0a6a67fca4ba71fb0c27c7dda45a
IrishFagan <irishfagan@live.com>
2022-10-25 11:22:54 -0700

    Refactor return body of manageFrameInventory

--- handler.js
+++ handler.js
@@ -1,4 +1,11 @@
 "use strict";
+const axios = require('axios');
+
+const getComponentCount = async (componentName) => {
+  return axios
+    .get(`https://www.njs-export.com/collections/${componentName}.json`)
+    .then(res => res.data.collection.products_count);
+}
 
 module.exports.hello = async (event) => {
   return {
@@ -7,6 +14,19 @@ module.exports.hello = async (event) => {
       {
         message: "Go Serverless v3.0! Your function executed successfully!",
         input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.manageFrameInventory = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        count: await getComponentCount('frames')
       },
       null,
       2
