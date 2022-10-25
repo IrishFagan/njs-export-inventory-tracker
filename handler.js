9687ea67e1c5aa95984a0e5a99c918695a7f84a7
IrishFagan <irishfagan@live.com>
2022-10-25 14:17:36 -0700

    Remove boilerplate 'hello' function

--- handler.js
+++ handler.js
@@ -18,20 +18,6 @@ const getComponentInfo = async (componentName) => {
   return frames;
 }
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
-
 module.exports.frameCount = async (event) => {
   return {
     statusCode: 200,
