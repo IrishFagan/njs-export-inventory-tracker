986935c310f0061fcda3ddbc8d355dea90308cd8
IrishFagan <irishfagan@live.com>
2022-10-25 18:20:15 -0700

    Duplicate existing functionality to handle more types of bike components

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ export default function App() {
   const [listingDate, setListingDate] = useState(new Date(/*2013-06-18'*/));
   const [frames, setFrames] = useState([]);
   const [chainrings, setChainrings] = useState([]);
-  const [stems, setStems] = useState([]);
   const [cranks, setCranks] = useState([]);
   const [hubs, setHubs] = useState([]);
+  const [stems, setStems] = useState([]);
+  const [handlebars, setHandlebars] = useState([]);
 
   const sortByDate = (components) => {
     return components.sort((a,b) => new Date(b.created_at) - new Date(a.created_at));
@@ -21,10 +22,15 @@ export default function App() {
     const getComponentInfo = async (componentName, component, setComponent) => {
       return axios
         .get(`https://v3ory7fu51.execute-api.us-west-2.amazonaws.com/${componentName}`)
-        .then(res => setComponent(res.data.frames));
+        .then(res => setComponent(res.data.componentData));
     }
 
     getComponentInfo('frames', frames, setFrames);
+    getComponentInfo('chainrings', chainrings, setChainrings);
+    getComponentInfo('cranks', cranks, setCranks);
+    getComponentInfo('hubs', hubs, setHubs);
+    getComponentInfo('stems', stems, setStems);
+    getComponentInfo('handlebars', handlebars, setHandlebars)
   }, [])
 
   return(
@@ -38,6 +44,31 @@ export default function App() {
       <ComponentList
         component={frames}
         componentName="Frames"
+        listingDate={listingDate}
+      />
+      <ComponentList
+        component={chainrings}
+        componentName="Chainrings"
+        listingDate={listingDate}
+      />
+      <ComponentList
+        component={cranks}
+        componentName="Cranks"
+        listingDate={listingDate}
+      />
+      <ComponentList
+        component={hubs}
+        componentName="Hubs"
+        listingDate={listingDate}
+      />
+      <ComponentList
+        component={stems}
+        componentName="Stems"
+        listingDate={listingDate}
+      />
+      <ComponentList
+        component={handlebars}
+        componentName="Handlebars"
         listingDate={listingDate}
       />
     </div>
