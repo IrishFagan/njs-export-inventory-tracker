5b601b1ef800e0fee80f823fea1369dea23878f4
IrishFagan <irishfagan@live.com>
2022-10-27 13:19:15 -0700

    Add functionality for date to be updated depending on most recent listing date

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ComponentList from './components/ComponentList';
 import DateSelector from './components/DateSelector';
 
 export default function App() {
-  const [listingDate, setListingDate] = useState(new Date('2022-10-27'));
+  const [listingDate, setListingDate] = useState(new Date('0000-01-01'));
   const [frames, setFrames] = useState([]);
   const [chainrings, setChainrings] = useState([]);
   const [cranks, setCranks] = useState([]);
@@ -13,16 +13,22 @@ export default function App() {
   const [stems, setStems] = useState([]);
   const [handlebars, setHandlebars] = useState([]);
 
-  const sortByDate = (components) => {
-    return components.sort((a,b) => new Date(b.created_at) - new Date(a.created_at));
-  }
+  const formatDate = (date) => new Date(date.replace(/-/g, '\/').replace(/T.+/, ''))
 
   useEffect(() => {
     
     const getComponentInfo = async (componentName, component, setComponent) => {
       return axios
         .get(`https://v3ory7fu51.execute-api.us-west-2.amazonaws.com/${componentName}`)
-        .then(res => setComponent(sortByDate(res.data.componentData)));
+        .then(res => setComponent(res.data.componentData));
+    }
+
+    const getLatestListingDate = async () => {
+      setListingDate(
+        await axios
+          .get('https://v3ory7fu51.execute-api.us-west-2.amazonaws.com/latest')
+          .then((res) => formatDate(res.data.latestListingDate))
+      )
     }
 
     getComponentInfo('frames', frames, setFrames);
@@ -30,7 +36,8 @@ export default function App() {
     getComponentInfo('cranks', cranks, setCranks);
     getComponentInfo('hubs', hubs, setHubs);
     getComponentInfo('stems', stems, setStems);
-    getComponentInfo('handlebars', handlebars, setHandlebars)
+    getComponentInfo('handlebars', handlebars, setHandlebars);
+    getLatestListingDate();
   }, [])
 
   return(
