8a9ce965d5c4a4d91d553d0db68f6bf277e25489
IrishFagan <irishfagan@live.com>
2022-11-03 17:20:57 -0700

    Modify component attribute access to reflect changes in the API

--- src/components/Component.js
+++ src/components/Component.js
@@ -2,22 +2,15 @@ import './../App.css'
 
 export default function Component({component, index, listingDate}) {
 
-	const CreatedDate = (component) => {
-	  const date = new Date(component.PutRequest.Item.CreatedDate.replace(/-/g, '\/').replace(/T.+/, '')).toDateString()
-	  return date;
-	}
-
-	if(CreatedDate(component) === listingDate) {
-		return(
-			<li className="list" key={index}>
-				<a href={`https://www.njs-export.com/products/${component.PutRequest.Item.Handle}`}>
-	  		  <img src={component.PutRequest.Item.Image} alt="Frame" width={250} height={200} />
-	  		</a>
-  		  <div>
-  		   	{component.PutRequest.Item.Title}
-  		  </div>
-  		  {component.PutRequest.Item.Available ? null : "SOLD OUT"}
-  		</li>
-		)
-	}
+	return(
+		<li className="list" key={index}>
+			<a href={`https://www.njs-export.com/products/${component.Handle}`}>
+	 		  <img src={component.Image} alt="Frame" width={250} height={200} />
+	 		</a>
+  	  <div>
+  	   	{component.Title}
+  	  </div>
+  	  {component.Available ? null : "SOLD OUT"}
+  	</li>
+	)
 }
