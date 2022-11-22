a608427dfdbe1b211407fe37ff0394e64dd91154
IrishFagan <irishfagan@live.com>
2022-11-21 19:33:07 -0800

    Add grid styling to component list

--- src/components/ComponentList.js
+++ src/components/ComponentList.js
@@ -3,7 +3,7 @@ import Component from './Component';
 export default function ComponentList({componentName, component, listingDate}) {
 	return(
 		<div>
-    	<ul className="grid grid-cols-3">
+    	<ul className="grid grid-cols-3 place-content-evenly gap-5 my-3 mx-8">
     	{component.map((individualComponent) =>
     	  <Component
     	  	key={individualComponent.ID}
