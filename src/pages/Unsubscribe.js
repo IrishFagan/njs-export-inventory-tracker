3a59e5a9cc8b0749a80896f946775eb300d58594
IrishFagan <irishfagan@live.com>
2022-12-18 15:51:42 -0800

    Add condition to handle empty query params

--- src/pages/Unsubscribe.js
+++ src/pages/Unsubscribe.js
@@ -8,18 +8,24 @@ export default function Unsubscribe() {
 
 	useEffect(() => {
 		const getKeywords = () => {
-			axios
-				.get(`https://api.njs.bike/unsubscribe?email=${queryParams.get('email').replace('@','%40')}`)
-				.then(res => setKeywords(res.data.body));
+			if(queryParams.get('email')) {
+				axios
+					.get(`https://api.njs.bike/unsubscribe?email=${queryParams.get('email').replace('@','%40')}`)
+					.then(res => setKeywords(res.data.body));
+			}
 		};
 
 		getKeywords();
 	}, [])
 
+
 	return (
+		queryParams.get('email') === null ? 
+		<h2 className="justify-center">Error: Missing parameter 'Email' - Please follow the provided link again</h2> :
 		<div>
 			<ul>
-				{keywords.map((keyword, i) =>
+				{keywords.length === 0 ? <li>loading...</li> :
+					keywords.map((keyword, i) =>
 					<li key={i}>
 						{keyword.keyword}
 					</li>
