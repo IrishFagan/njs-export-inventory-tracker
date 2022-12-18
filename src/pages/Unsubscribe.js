cf5facc79006fbfc3551bde87baac2bdb88805e9
IrishFagan <irishfagan@live.com>
2022-12-18 15:33:43 -0800

    Add functionality to retrieve and display keywords associated with email

--- src/pages/Unsubscribe.js
+++ src/pages/Unsubscribe.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import axios from 'axios'
 
 export default function Unsubscribe() {
 	const [queryParams] = useSearchParams();
+	const [keywords, setKeywords] = useState([]);
 
-	console.log(queryParams.get('email'))
+	useEffect(() => {
+		const getKeywords = () => {
+			axios
+				.get(`https://api.njs.bike/unsubscribe?email=${queryParams.get('email').replace('@','%40')}`)
+				.then(res => setKeywords(res.data.body));
+		};
+
+		getKeywords();
+	}, [])
 
 	return (
 		<div>
 			<ul>
-				List of keywords :)
+				{keywords.map((keyword, i) =>
+					<li key={i}>
+						{keyword.keyword}
+					</li>
+				)}
 			</ul>
 		</div>
 	)
