e8b0475459cf6237ff0f1a4a4f393650c8c11ce1
IrishFagan <irishfagan@live.com>
2022-12-18 13:33:37 -0800

    Create 'Header' component for reusability across routes

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import './../App.css'
+
+export default function Header() {
+	return (
+		<div>
+			<h1 className="inline flex justify-center text-5xl italic m-4">
+  	  	<a className="header-link" href="https://njs.bike">
+  	      NJS Export Inventory Tracker
+  	    </a>
+  	  </h1>
+		</div>
+	)
+}
