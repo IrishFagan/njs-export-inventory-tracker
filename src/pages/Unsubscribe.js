62845ed89ec2b454b1016ef961fe29ec8394c312
IrishFagan <irishfagan@live.com>
2022-12-18 14:17:39 -0800

    Add functionality to access query params from url

--- src/pages/Unsubscribe.js
+++ src/pages/Unsubscribe.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Unsubscribe() {
+	const [queryParams] = useSearchParams();
 
+	console.log(queryParams.get('email'))
 
 	return (
 		<div>
 			<ul>
-				List of keywords
+				List of keywords :)
 			</ul>
 		</div>
 	)
