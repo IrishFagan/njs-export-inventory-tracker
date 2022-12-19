14550fed4f34f004948cc00bc0c005818820b1e3
IrishFagan <irishfagan@live.com>
2022-12-18 16:49:47 -0800

    Reimplement 'unsubscribe' function to unsubscribe from certain given keywords instead of all keywords

--- handler.js
+++ handler.js
@@ -200,6 +200,45 @@ const getSubscriptionEmails = async (keyword) => {
 
 /* - HANDLER FUNCTIONS - */
 
+module.exports.unsubscribe = async (event) => {
+  const email = event['queryStringParameters']['email']
+  const hash = event['queryStringParameters']['hash']
+  const keywords = event['queryStringParameters']['keywords']
+  const params = {
+    TableName: 'UserHashTable',
+    IndexName: 'userHashIndex',
+    KeyConditionExpression: 'UserHash = :uh',
+    FilterExpression: '#timetolive >= :currentEpoch',
+    ExpressionAttributeValues: {
+      ':uh': parseInt(event['queryStringParameters']['hash']),
+      ':currentEpoch': Date.now() / 1000
+    },
+    ExpressionAttributeNames: {
+      '#timetolive': 'ttl'
+    }
+  }
+
+  const emailHash = await db.query(params).promise()
+  
+  if(emailHash.Count) {
+    for (let keyword of keywords) {
+      await queryDB(`
+        DELETE subscriptions
+        FROM subscriptions
+        INNER JOIN emails
+        ON emails.email_id = subscriptions.email_id_fk
+        INNER JOIN keywords
+        ON keywords.keyword_id = subscriptions.keyword_id_fk
+        WHERE emails.email = ${escape(email)}
+        AND keywords.keyword = ${escape(keyword)};
+      `)
+    }
+    return jsonResponse(200, 'Successfully unsubscribed from selected keywords!')
+  } else {
+    return jsonResponse(404, 'Your link is dead. Please request another unsubscribe email and fill out the form within a couple of minutes')
+  }
+}
+
 module.exports.getKeywords = async (event) => {
   const email = event['queryStringParameters']['email'];
   const subscriptions = await queryDB(`
